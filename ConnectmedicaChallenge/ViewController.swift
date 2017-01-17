0225917f176c048167c3477150c52034417a26ca
Konrad Zdunczyk <konrad.zdunczyk@gmail.com>
2017-01-17 15:32:52 +0100

    Initial Commit

---
+++ ConnectmedicaChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectmedicaChallenge
+//
+//  Created by Konrad Zdunczyk on 17/01/17.
+//  Copyright © 2017 Konrad Zdunczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
